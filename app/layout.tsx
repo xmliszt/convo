d09cb1bd5f75f03808693d78cca4bf538c11cc21
Li Yuxuan <xmliszt@gmail.com>
2024-04-14 05:23:09 +0800

    light dark theme viewport. prevent scalable

--- app/layout.tsx
+++ app/layout.tsx
@@ -75,7 +75,11 @@ export const metadata: Metadata = {
 export const viewport: Viewport = {
   width: 'device-width',
   initialScale: 1,
-  themeColor: '#000000',
+  themeColor: [
+    { media: '(prefers-color-scheme: light)', color: 'white' },
+    { media: '(prefers-color-scheme: dark)', color: 'black' },
+  ],
+  userScalable: false,
 };
 
 const font = Bodoni_Moda({
