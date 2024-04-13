1cba6dbea17f6fe3c2a5149068465f5a8eec5662
Li Yuxuan <xmliszt@gmail.com>
2024-04-13 18:09:44 +0800

    Disable prodution chat

--- app/page.tsx
+++ app/page.tsx
@@ -4,7 +4,13 @@ export default function Home() {
   return (
     <main className='flex h-full w-full flex-col items-center gap-y-4'>
       <h1 className='mx-auto max-w-lg p-4 text-center'>Convo</h1>
-      <Chat />
+      {process.env.VERCEL_ENV === 'production' ? (
+        <div className='p-4 text-center'>
+          <p>🚧 This is a work in progress. Be right back!</p>
+        </div>
+      ) : (
+        <Chat />
+      )}
     </main>
   );
 }
