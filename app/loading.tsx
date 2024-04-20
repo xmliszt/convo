a10680f2cd96e761d28cb127b17df52dc58200e6
Li Yuxuan <xmliszt@gmail.com>
2024-04-20 14:11:09 +0800

    Add convo logo loader, shorten group drawer

---
+++ app/loading.tsx
@@ -0,0 +1,16 @@
+import { ConvoLoader } from '@/lib/convo-loader';
+
+export default function Loading() {
+  return (
+    <main className='relative h-full w-full'>
+      <div
+        className='absolute left-0 top-0 z-[998] flex h-full w-full items-center justify-center backdrop-blur-[10px]'
+        style={{
+          WebkitBackdropFilter: 'blur(10px)',
+        }}
+      >
+        <ConvoLoader />
+      </div>
+    </main>
+  );
+}
